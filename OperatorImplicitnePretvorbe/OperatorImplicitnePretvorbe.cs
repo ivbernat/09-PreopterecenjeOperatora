0470c16d370b28e4c18fd71064c3e2a181186de7
Ivan Bernatović <ivbernat@vsite.hr>
2016-12-08 20:22:05 +0100

    implicitna pretvorba

--- OperatorImplicitnePretvorbe/OperatorImplicitnePretvorbe.cs
+++ OperatorImplicitnePretvorbe/OperatorImplicitnePretvorbe.cs
@@ -18,13 +18,13 @@ namespace Vsite.CSharp
             {
                 Racionalni rac = new Racionalni();
                 // koristi implicitnu pretvorbu iz double
-                //rac = 0.1;
-                //Debug.Assert(rac.ToString() == "1 / 10");
+                rac = 0.1;
+                Debug.Assert(rac.ToString() == "1 / 10");
                 Console.WriteLine(rac);
 
                 // koristi implicitnu pretvorbu iz int
-                //rac = 2;
-                //Debug.Assert(rac.ToString() == "2 / 1");
+                rac = 2;
+               Debug.Assert(rac.ToString() == "2 / 1");
                 Console.WriteLine(rac);
             }
             catch (System.Exception e)
